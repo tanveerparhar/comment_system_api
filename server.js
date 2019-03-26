591309b7052ee1997cd6290a842df08a7391efdd
Tanveer Singh <31854574+tanveerparhar@users.noreply.github.com>
2019-03-27 05:17:07 +0530

    editing databse credentials

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const db=knex({
   client: 'pg',
   version: '7.2',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '100parhar',
-    database : 'commentsystemdb'
+    host : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
